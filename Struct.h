3aac8e3f54e765f55bb09a2d3e6d47dbad98145a
yoshizawa-katsuya <k023g0036@g.neec.ac.jp>
2024-01-26 10:38:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Struct.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+
+typedef struct intVector2 {
+	int x;
+	int y;
+}intVector2;
+
+typedef struct Vector2 {
+	float x;
+	float y;
+}Vector2;
+
+typedef struct Matrix3x3
+{
+	float m[3][3];
+} Matrix3x3;
